2ca8104f228fed07fa4009874ab09b4ee1e02041
cool boy <soknao2003@gmail.com>
2025-03-30 21:37:56 +0700

    more

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,10 @@ use App\Http\Controllers\ProductController;
 
 require __DIR__.'/auth.php';
 
-Route::get('/', function (){
-    return view('login');
-});
+ 
 
  
-Route::resource('logo', LogoController::class);
+Route::resource('/', LogoController::class);
 Route::resource('slider', SilderController::class);
 Route::resource('banner', BannerController::class);
 Route::resource('products', ProductController::class);
